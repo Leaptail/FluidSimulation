723aea49ec1efce3e8bcf47e1e4a253f2cfb00f9
Leaptail <52641374+Leaptail@users.noreply.github.com>
2024-05-15 21:47:02 -0700

    I did it!!!

--- NavierStokes.py
+++ NavierStokes.py
@@ -42,17 +42,25 @@ DENSITY = 1.0
 HORIZONTAL_VELOCITY_TOP = 1.0
 
 N_PRESSURE_POISSON_ITERATIONS = 50
+STABILITY_SAFETY_FACTOR = 0.5
 
 def main():
     element_length = DOMAIN_SIZE / (N_POINTS - 1)
+
+    #linspace makes linear spaced vectors (points) from (start number, end number, no of points)
     x = np.linspace(0.0, DOMAIN_SIZE, N_POINTS)
     y = np.linspace(0.0, DOMAIN_SIZE, N_POINTS)
 
+    #
     X, Y = np.meshgrid(x, y)
 
+    #make a 2D array of zeros, (Npoints by Npoints)
     u_prev = np.zeros_like(X)
     v_prev = np.zeros_like(X)
     p_prev = np.zeros_like(X)
+
+    #print(u_prev)
+    #print(Y.shape)
 
     #central difference is the best estimated approximation of the differentiated value at x
     def central_difference_x(f):
@@ -70,7 +78,7 @@ def main():
 
     def central_difference_y(f):
         diff = np.zeros_like(f)
-        diff[1: -1, 1: -1] = (
+        diff[1:-1, 1:-1] = (
             f[2:  , 1:-1]
             -
             f[0:-2, 1:-1]
@@ -78,6 +86,174 @@ def main():
             2 * element_length
         )
         return diff
+
+    def laplace(f):
+        diff = np.zeros_like(f)
+        diff[1:-1, 1:-1] = (
+            f[1:-1, 0:-2]
+            +
+            f[0:-2, 1:-1]
+            -
+            4
+            *
+            f[1:-1, 1:-1]
+            +
+            f[1:-1, 2:  ]
+            +
+            f[2:  , 1:-1]
+        ) / (
+            element_length**2
+        )
+        return diff
+    
+
+    maximum_possible_time_step_length = (
+        0.5 * element_length**2 / KINEMATIC_VISCOSITY
+    )
+
+    if TIME_STEP_LENGTH > STABILITY_SAFETY_FACTOR * maximum_possible_time_step_length:
+        raise RuntimeError("Stability is not guaranteed")
+
+    for _ in tqdm(range(N_ITERATIONS)):
+        d_u_prev__d_x = central_difference_x(u_prev)
+        d_u_prev__d_y = central_difference_y(u_prev)
+        d_v_prev__d_x = central_difference_x(v_prev)
+        d_v_prev__d_y = central_difference_y(v_prev)
+        laplace__u_prev = laplace(u_prev)
+        laplace__v_prev = laplace(v_prev)
+
+        # perform a tentative step by solving the momentum equations without the pressure gradient
+        u_tent = (
+            u_prev
+            +
+            TIME_STEP_LENGTH * (
+                -
+                (
+                    u_prev * d_u_prev__d_x
+                    +
+                    v_prev * d_u_prev__d_y
+                )
+                +
+                KINEMATIC_VISCOSITY * laplace__u_prev
+            )
+        )
+        v_tent = (
+            v_prev
+            +
+            TIME_STEP_LENGTH * (
+                - 
+                (
+                    u_prev * d_v_prev__d_x
+                    +
+                    v_prev * d_v_prev__d_y
+                )
+                +
+                KINEMATIC_VISCOSITY * laplace__v_prev
+            )
+        )
+
+        #Velocity Boundary Conditions : Homogenous Dirichlet BC everywhere except 
+        # for the horizontal velocity at the top, which is prescribed
+        u_tent[0, :] = 0.0
+        u_tent[:, 0] = 0.0
+        u_tent[:, -1] = 0.0
+        u_tent[-1, :] = HORIZONTAL_VELOCITY_TOP
+        v_tent[0, :] = 0.0
+        v_tent[:, 0] = 0.0
+        v_tent[:, -1] = 0.0
+        v_tent[-1, :] = 0.0
+
+
+        d_u_tent__d_x = central_difference_x(u_tent)
+        d_v_tent__d_y = central_difference_y(v_tent)
+
+        # Compute a pressure correction by solving the pressure-poisson equation
+        rhs = (
+            DENSITY / TIME_STEP_LENGTH
+            *
+            (
+                d_u_tent__d_x
+                +
+                d_v_tent__d_y
+            )
+        )
+
+        for _ in range(N_PRESSURE_POISSON_ITERATIONS):
+            p_next = np.zeros_like(p_prev)
+            p_next[1:-1, 1:-1] = 1/4 * (
+                +
+                p_prev[1:-1, 0:-2]
+                +
+                p_prev[0:-2, 1:-1]
+                +
+                p_prev[1:-1, 2:  ]
+                +
+                p_prev[2:  , 1:-1]
+                -
+                element_length**2
+                *
+                rhs[1:-1, 1:-1]
+            )
+            
+            # Pressure Boundary Conditinos: Homogenous Neumann Boundary 
+            # Conditions everywhere except for the top, where it is a homogenous Dirichlet BC
+            p_next[:, -1] = p_next[:, -2]
+            p_next[0,  :] = p_next[1,  :]
+            p_next[:,  0] = p_next[:,  1]
+            p_next[-1, :] = 0.0
+            
+            p_prev = p_next
+
+
+        d_p_next__d_x = central_difference_x(p_next)
+        d_p_next__d_y = central_difference_y(p_next)
+
+        # Correct the velocities such that the fluid stays incompressible
+        u_next = (
+            u_tent
+            -
+            TIME_STEP_LENGTH / DENSITY
+            *
+            d_p_next__d_x
+        )
+        v_next = (
+            v_tent
+            -
+            TIME_STEP_LENGTH / DENSITY
+            *
+            d_p_next__d_y
+        )
+
+
+        #Velocity Boundary Conditions : Homogenous Dirichlet BC everywhere except 
+        # for the horizontal velocity at the top, which is prescribed
+        u_next[0, :] = 0.0
+        u_next[:, 0] = 0.0
+        u_next[:, -1] = 0.0
+        u_next[-1, :] = HORIZONTAL_VELOCITY_TOP
+        v_next[0, :] = 0.0
+        v_next[:, 0] = 0.0
+        v_next[:, -1] = 0.0
+        v_next[-1, :] = 0.0
+
+
+        #Advance in time
+        u_prev = u_next
+        v_prev = v_next
+        p_prev = p_next
+
+
+
+    plt.style.use("dark_background")
+    plt.figure()
+    plt.contourf(X[::2, ::2], Y[::2, ::2], p_next[::2, ::2], cmap="coolwarm")
+    plt.colorbar()
+
+    plt.quiver(X[::2, ::2], Y[::2, ::2], u_next[::2, ::2], v_next[::2, ::2], color="black")
+    # plt.streamplot(X[::2, ::2], Y[::2, ::2], u_next[::2, ::2], v_next[::2, ::2], color="black")
+    plt.xlim((0, 1))
+    plt.ylim((0, 1))
+    plt.show()
 
 if __name__ == "__main__":
     main()
